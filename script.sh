c2e6c1761b5800a53a0c0769f7dda34f27b1fd6d
Hantian Zhang <hanzhang@sgs-gpu-01.inf.ethz.ch>
2016-11-25 20:43:46 +0100

    add path to parameters

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ for a in 1.4 1.8
 do
     for n in 1.0 1.2 2.0 5.0 10.0
     do
-        DATA_ROOT=/mnt/ds3lab/astronomy/001002_${a}_${n}/jpg/ name=001002_${a}_${n} which_direction=BtoA display=0 niter=20 batchSize=1 gpu=1 th train.lua
+        DATA_ROOT=/mnt/ds3lab/astronomy/001002_${a}_${n}/jpg/ name=001002_${a}_${n} which_direction=BtoA display=0 niter=20 batchSize=1 gpu=1 checkpoints_dir=/mnt/ds3lab/roou_v2_checkpoint/ th train.lua
     done
 done
 
@@ -13,7 +13,7 @@ for a in 2.2 2.5
 do
     for n in 1.0 1.2 2.0 5.0 10.0
     do
-        DATA_ROOT=/mnt/ds3lab/astronomy/001002_${a}_${n}/jpg/ name=001002_${a}_${n} which_direction=BtoA display=0 niter=20 batchSize=1 gpu=2 th train.lua
+        DATA_ROOT=/mnt/ds3lab/astronomy/001002_${a}_${n}/jpg/ name=001002_${a}_${n} which_direction=BtoA display=0 niter=20 batchSize=1 gpu=2 checkpoints_dir=/mnt/ds3lab/roou_v2_checkpoint/ th train.lua
     done
 done
 
@@ -24,7 +24,7 @@ for a in 1.4 1.8
 do
     for n in 1.0 1.2 2.0 5.0 10.0
     do
-    	DATA_ROOT=/mnt/ds3lab/astronomy/001002_${a}_${n}/jpg/ name=001002_${a}_${n} which_direction=BtoA phase=test display=0 gpu=2 th test.lua
+    	DATA_ROOT=/mnt/ds3lab/astronomy/001002_${a}_${n}/jpg/ name=001002_${a}_${n} which_direction=BtoA phase=test display=0 gpu=2 checkpoints_dir=/mnt/ds3lab/roou_v2_checkpoint/ results_dir=/mnt/ds3lab/roou_v2_results/ th test.lua
     done
 done
 
@@ -33,7 +33,7 @@ for a in 2.2 2.5
 do
     for n in 1.0 1.2 2.0 5.0  10.0
     do
-    	DATA_ROOT=/mnt/ds3lab/astronomy/001002_${a}_${n}/jpg/ name=001002_${a}_${n} which_direction=BtoA phase=test display=0 gpu=1 th test.lua
+    	DATA_ROOT=/mnt/ds3lab/astronomy/001002_${a}_${n}/jpg/ name=001002_${a}_${n} which_direction=BtoA phase=test display=0 gpu=1 checkpoints_dir=/mnt/ds3lab/roou_v2_checkpoint/ results_dir=/mnt/ds3lab/roou_v2_results/ th test.lua
     done
 done
 
